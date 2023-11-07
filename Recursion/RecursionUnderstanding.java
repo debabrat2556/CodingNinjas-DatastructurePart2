201672cbe30b911e64f1016f4666a5f049b7dc50
debabrat2556 <debabrat863827@gmail.com>
2023-11-07 12:55:48 +0530

    comment added to understand recursion program

--- Recursion/RecursionUnderstanding.java
+++ Recursion/RecursionUnderstanding.java
@@ -1,3 +1,5 @@
+// program to find sum of n natural number using recursion
+
 package CodingNinjas_DatastructurePart2.Recursion;
 
 public class RecursionUnderstanding {
@@ -12,8 +14,17 @@ public class RecursionUnderstanding {
         {
             return 0;
         }
-        int smallValue=sumn(n-1);
+
+        // for the above if section we are certain that the function return exact
+        // value when n value stands for its minimum
+
+        int smallValue=sumn(n-1); 
+        //in the above statement we are assuming the function stands true for n-1 value
+
         int output=n+smallValue;
+        //in the above statement we are proving that we will get the desired output
+        // by adding n to sum(n-1) (which we have proved in the above line of this)
+
         return output;
     }
 }
