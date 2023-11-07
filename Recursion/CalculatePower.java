a71cb494a5619f4e7f899af6755d5c739c57ca2f
debabrat2556 <debabrat863827@gmail.com>
2023-11-08 00:02:50 +0530

    understanding recursion while calculating power for a given number

--- Recursion/CalculatePower.java
+++ Recursion/CalculatePower.java
@@ -2,16 +2,18 @@ package CodingNinjas_DatastructurePart2.Recursion;
 
 public class CalculatePower {
     public static void main(String[] args) {
-        System.out.println(power(3, 3));
+        System.out.println(power(3, 4));
+        //passing the value to the function from the main function
     }
     public static int power(int x, int n) 
     {
         if(n==0)
         {
-            return 1;
+            return 1;       //as we know x to the power 0 is 1
+                            //so if n value remains 0 the return value would become 1
         }
         int smallOutPut=power(x, (n-1));
-        int output=x*smallOutPut;
+        int output=x*smallOutPut;   //which simply means x*(int)Math.pow(x, n-1)
         return output;
     }
 }
